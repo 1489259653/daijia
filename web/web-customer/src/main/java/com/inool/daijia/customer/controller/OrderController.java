cc8eb36cf1497159dbcae04330bfc03c213af852
1489259653 <1489259653eric@gmail.com>
2025-03-22 15:12:17 +0800

    预估订单数据接口

--- web/web-customer/src/main/java/com/inool/daijia/customer/controller/OrderController.java
+++ web/web-customer/src/main/java/com/inool/daijia/customer/controller/OrderController.java
@@ -2,14 +2,15 @@ package com.inool.daijia.customer.controller;
 
 import com.inool.daijia.common.login.InoolLogin;
 import com.inool.daijia.common.result.Result;
+import com.inool.daijia.customer.service.OrderService;
+import com.inool.daijia.model.form.customer.ExpectOrderForm;
+import com.inool.daijia.model.vo.customer.ExpectOrderVo;
 import com.inool.daijia.model.vo.order.CurrentOrderInfoVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Tag(name = "订单API接口管理")
@@ -24,6 +25,16 @@ public class OrderController {
         CurrentOrderInfoVo currentOrderInfoVo = new CurrentOrderInfoVo();
         currentOrderInfoVo.setIsHasCurrentOrder(false);
         return Result.ok(currentOrderInfoVo);
+    }
+
+    @Autowired
+    private OrderService orderService;
+
+    @Operation(summary = "预估订单数据")
+    @InoolLogin
+    @PostMapping("/expectOrder")
+    public Result<ExpectOrderVo> expectOrder(@RequestBody ExpectOrderForm expectOrderForm) {
+        return Result.ok(orderService.expectOrder(expectOrderForm));
     }
 
 }
