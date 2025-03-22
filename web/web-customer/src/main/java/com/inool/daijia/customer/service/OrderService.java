0c1a63cdfbd0e00f65b81d60d116d6e9d1401486
1489259653 <1489259653eric@gmail.com>
2025-03-22 15:12:17 +0800

    预估订单数据接口

--- web/web-customer/src/main/java/com/inool/daijia/customer/service/OrderService.java
+++ web/web-customer/src/main/java/com/inool/daijia/customer/service/OrderService.java
@@ -1,5 +1,9 @@
 package com.inool.daijia.customer.service;
 
+import com.inool.daijia.model.form.customer.ExpectOrderForm;
+import com.inool.daijia.model.vo.customer.ExpectOrderVo;
+
 public interface OrderService {
 
+    ExpectOrderVo expectOrder(ExpectOrderForm expectOrderForm);
 }
