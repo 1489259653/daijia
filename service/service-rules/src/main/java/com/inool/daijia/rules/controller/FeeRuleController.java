67247e80056bf7beffb33dfdc0287d3398cde8ca
1489259653 <1489259653eric@gmail.com>
2025-03-22 14:58:53 +0800

    封装规则接口

--- service/service-rules/src/main/java/com/inool/daijia/rules/controller/FeeRuleController.java
+++ service/service-rules/src/main/java/com/inool/daijia/rules/controller/FeeRuleController.java
@@ -1,7 +1,14 @@
 package com.inool.daijia.rules.controller;
 
+import com.inool.daijia.common.result.Result;
+import com.inool.daijia.model.form.rules.FeeRuleRequestForm;
+import com.inool.daijia.model.vo.rules.FeeRuleResponseVo;
+import com.inool.daijia.rules.service.FeeRuleService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rules/fee")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class FeeRuleController {
+    @Autowired
+    private FeeRuleService feeRuleService;
 
+    @Operation(summary = "计算订单费用")
+    @PostMapping("/calculateOrderFee")
+    public Result<FeeRuleResponseVo> calculateOrderFee(@RequestBody FeeRuleRequestForm calculateOrderFeeForm) {
+        return Result.ok(feeRuleService.calculateOrderFee(calculateOrderFeeForm));
+    }
 
 
 }
--- service/service-rules/src/main/java/com/inool/daijia/rules/service/FeeRuleService.java
+++ service/service-rules/src/main/java/com/inool/daijia/rules/service/FeeRuleService.java
@@ -1,5 +1,9 @@
 package com.inool.daijia.rules.service;
 
+import com.inool.daijia.model.form.rules.FeeRuleRequestForm;
+import com.inool.daijia.model.vo.rules.FeeRuleResponseVo;
+
 public interface FeeRuleService {
 
+    FeeRuleResponseVo calculateOrderFee(FeeRuleRequestForm calculateOrderFeeForm);
 }
