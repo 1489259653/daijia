a0958b8c867b66bda8e445913722f6df31c51e53
1489259653 <1489259653eric@gmail.com>
2025-03-23 10:51:55 +0800

    搜索附近适合接单的司机

--- service-client/service-map-client/src/main/java/com/inool/daijia/map/client/LocationFeignClient.java
+++ service-client/service-map-client/src/main/java/com/inool/daijia/map/client/LocationFeignClient.java
@@ -1,12 +1,16 @@
 package com.inool.daijia.map.client;
 
 import com.inool.daijia.common.result.Result;
+import com.inool.daijia.model.form.map.SearchNearByDriverForm;
 import com.inool.daijia.model.form.map.UpdateDriverLocationForm;
+import com.inool.daijia.model.vo.map.NearByDriverVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(value = "service-map")
 public interface LocationFeignClient {
@@ -26,4 +30,11 @@ public interface LocationFeignClient {
     @DeleteMapping("/map/location/removeDriverLocation/{driverId}")
     Result<Boolean> removeDriverLocation(@PathVariable("driverId") Long driverId);
 
+    /**
+     * 搜索附近满足条件的司机
+     * @param searchNearByDriverForm
+     * @return
+     */
+    @PostMapping("/map/location/searchNearByDriver")
+    Result<List<NearByDriverVo>> searchNearByDriver(@RequestBody SearchNearByDriverForm searchNearByDriverForm);
 }
--- service/service-map/src/main/java/com/inool/daijia/map/service/LocationService.java
+++ service/service-map/src/main/java/com/inool/daijia/map/service/LocationService.java
@@ -1,10 +1,16 @@
 package com.inool.daijia.map.service;
 
+import com.inool.daijia.model.form.map.SearchNearByDriverForm;
 import com.inool.daijia.model.form.map.UpdateDriverLocationForm;
+import com.inool.daijia.model.vo.map.NearByDriverVo;
+
+import java.util.List;
 
 public interface LocationService {
 
     Boolean updateDriverLocation(UpdateDriverLocationForm updateDriverLocationForm);
 
     Boolean removeDriverLocation(Long driverId);
+
+    List<NearByDriverVo> searchNearByDriver(SearchNearByDriverForm searchNearByDriverForm);
 }
